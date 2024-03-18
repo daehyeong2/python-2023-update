f1b2d017a988a136daabbc1c1b288b643108a866
daehyeong2 <baconbacon1231@gmail.com>
2024-03-18 22:09:50 +0900

    jobs 리스트에서 모든 li 추출 (6.0~6.2)

--- main.py
+++ main.py
@@ -1,56 +1,12 @@
-class Player:
-    def __init__(self, name, team):
-        self.name = name
-        self.team = team
-        self.xp = 5
-    def introduce(self):
-        print(f"Hello my name is {self.name} and I play for {self.team}")
+import requests
+from bs4 import BeautifulSoup
 
-class Team:
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.players = []
-    def add_player(self, name):
-        new_player = Player(name=name, team=self.team_name)
-        self.players.append(new_player)
-    def remove_player(self, name):
-        for player in self.players:
-            if player.name == name:
-                self.players.remove(player)
-                break
-    def show_players(self):
-        for player in self.players:
-            player.introduce()
-    def show_xp(self):
-        xp = 0
-        for player in self.players:
-            xp+=player.xp
-        print(f"{self.team_name}'s xp is {xp}")
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-gorani = Player(
-    name="Gorani",
-    team="Team X"
-)
+response = requests.get(url)
 
-team_X = Team("Team X")
+soup = BeautifulSoup(response.content, "html.parser")
 
-team_X.add_player("Gorani")
-team_X.add_player("Gorani2")
-team_X.show_players()
+jobs = soup.find("section", {"class": "jobs"}).find_all("li")
 
-team_Blue = Team("Team Blue")
-
-team_Blue.add_player("Monkey")
-team_Blue.show_players()
-
-print("--remove Gorani2")
-
-team_X.remove_player("Gorani2")
-
-team_X.show_players()
-team_Blue.show_players()
-
-print("--sum of xp")
-
-team_X.show_xp()
-team_Blue.show_xp()
+print(jobs)
