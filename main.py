a936520b819d7254b2982b5b630d3216eca6a722
daehyeong2 <baconbacon1231@gmail.com>
2024-03-20 15:50:42 +0900

    url 정보 추가, 복습 (6.4)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+base_url = "https://weworkremotely.com"
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
 response = requests.get(url)
@@ -9,9 +10,19 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 jobs = soup.find("section", {"class": "jobs"}).find_all("li")[1:-1]
 
+all_jobs = []
+
 for job in jobs:
     title = job.find("span", {"class": "title"}).text
     company, position, region = job.find_all("span", {"class": "company"})
-    company = company.text
-    position = position.text
-    region = region.text
+    url = job.find("div", {"class": "tooltip--flag-logo"}).next_sibling["href"]
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "position": position.text,
+        "region": region.text,
+        "url": base_url + url
+    }
+    all_jobs.append(job_data)
+
+print(all_jobs)
