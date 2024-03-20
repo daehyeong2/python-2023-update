c7e8a831d0f001d502f73cd2851dd953fe83026a
daehyeong2 <baconbacon1231@gmail.com>
2024-03-20 15:27:53 +0900

    jobs 리스트 정리, 데이터 추출 (6.3)

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-jobs = soup.find("section", {"class": "jobs"}).find_all("li")
+jobs = soup.find("section", {"class": "jobs"}).find_all("li")[1:-1]
 
-print(jobs)
+for job in jobs:
+    title = job.find("span", {"class": "title"}).text
+    company, position, region = job.find_all("span", {"class": "company"})
+    company = company.text
+    position = position.text
+    region = region.text
