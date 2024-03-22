bb3e63c9ee0cd49984b0bee5c8e25c4e2b2f2541
daehyeong2 <baconbacon1231@gmail.com>
2024-03-22 13:51:09 +0900

    class 형태로 변경 (6.13)

--- dynamicScrapper.py
+++ dynamicScrapper.py
@@ -3,51 +3,57 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
-p = sync_playwright().start()
+class Scrapper:
+    def __init__(self):
+        self.p = sync_playwright().start()
+        self.browser = self.p.chromium.launch(headless=False)
+    def scrape_page(self, keyword, scroll_count=3, scroll_delay=0.5):
+        if isinstance(keyword, list) == False:
+            raise Exception("keyword는 배열이여야 합니다.")
+        for search_keyword in keyword:
+            page = self.browser.new_page()
+            page.goto(f"https://www.wanted.co.kr/search?query={search_keyword}&tab=position")
 
-browser = p.chromium.launch(headless=False)
+            for i in range(scroll_count):
+                page.keyboard.down("End")
+                time.sleep(scroll_delay)
 
-def scrape_page(keyword, scroll_count=3):
-    for search_keyword in keyword:
-        page = browser.new_page()
-        page.goto(f"https://www.wanted.co.kr/search?query={search_keyword}&tab=position")
+            content = page.content()
 
-        for i in range(scroll_count):
-            page.keyboard.down("End")
-            time.sleep(0.5)
+            page.close()
 
-        content = page.content()
+            soup = BeautifulSoup(content, "html.parser")
 
-        page.close()
+            jobs = soup.find_all("div", {"class": "JobCard_container__FqChn"})
 
-        soup = BeautifulSoup(content, "html.parser")
+            jobs_db = []
 
-        jobs = soup.find_all("div", {"class": "JobCard_container__FqChn"})
+            for job in jobs:
+                link = f"https://wanted.co.kr{job.find('a')['href']}"
+                title = job.find("strong", {"class": "JobCard_title__ddkwM"}).text
+                company_name = job.find("span", {"class": "JobCard_companyName__vZMqJ"}).text
+                reward = job.find("span", {"class": "JobCard_reward__sdyHn"}).text
+                job = {
+                    "title": title,
+                    "company_name": company_name,
+                    "reward": reward,
+                    "link": link
+                }
+                jobs_db.append(job)
 
-        jobs_db = []
+            file = open(f"{search_keyword}.csv", "w")
+            writer = csv.writer(file)
+            writer.writerow(["Title", "Company", "Reward", "Link"])
 
-        for job in jobs:
-            link = f"https://wanted.co.kr{job.find('a')['href']}"
-            title = job.find("strong", {"class": "JobCard_title__ddkwM"}).text
-            company_name = job.find("span", {"class": "JobCard_companyName__vZMqJ"}).text
-            reward = job.find("span", {"class": "JobCard_reward__sdyHn"}).text
-            job = {
-                "title": title,
-                "company_name": company_name,
-                "reward": reward,
-                "link": link
-            }
-            jobs_db.append(job)
+            for job in jobs_db:
+                writer.writerow(job.values())
 
-        file = open(f"{search_keyword}.csv", "w")
-        writer = csv.writer(file)
-        writer.writerow(["Title", "Company", "Reward", "Link"])
+            file.close()
+    def close(self):
+        self.p.stop()
 
-        for job in jobs_db:
-            writer.writerow(job.values())
+scrapper = Scrapper()
 
-        file.close()
+scrapper.scrape_page(["python", "react"], 5, 1)
 
-scrape_page(["python", "react"], 5)
-
-p.stop()
+scrapper.close()
